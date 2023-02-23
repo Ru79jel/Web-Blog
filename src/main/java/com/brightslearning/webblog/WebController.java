1c86c8d185fe202ce43119b68fbef7c16a3acb6e
Ru79jel <vera-josefine.moultrie@brightstraining.com>
2023-02-23 10:39:05 +0100

    first commit

---
+++ src/main/java/com/brightslearning/webblog/WebController.java
@@ -0,0 +1,13 @@
+package com.brightslearning.webblog;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class WebController {
+    @GetMapping("/")
+    public @ResponseBody String hello() {
+        return "hello";
+    }
+}
