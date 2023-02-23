79b9e67b5a107c73bf365cbbd557324f9a107bbb
Minh <martin.tran@hotmail.de>
2023-02-23 12:11:03 +0100

    ADDED: PostRepo UserRepo

---
+++ src/main/java/com/brightslearning/webblog/PostRepo.java
@@ -0,0 +1,10 @@
+package com.brightslearning.webblog;
+
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface PostRepo extends CrudRepository<BlogPost,Long> {
+
+    //List<BlogPost> findByNameStartingWith(String prefix);
+}
