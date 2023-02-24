689fc06996654a4fccb45349de6851a4de5f270f
MarcRoggee <Marc.Roggee@brightstraining@.com>
2023-02-24 11:52:31 +0100

    Adjusted BlogUser (Constructors, added password) and UserRepo(boolean existName) for registration process

--- src/main/java/com/brightslearning/webblog/UserRepo.java
+++ src/main/java/com/brightslearning/webblog/UserRepo.java
@@ -3,4 +3,6 @@ package com.brightslearning.webblog;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepo extends CrudRepository<BlogUser,Long> {
+
+    boolean existsByUserName(String userName);
 }
