9a774f1a2adcfee333b6c9e2cb3f9679e8bdebe5
MarcRoggee <Marc.Roggee@brightstraining@.com>
2023-02-27 10:08:29 +0100

    Update @notworking

--- src/main/java/com/brightslearning/webblog/AdminController.java
+++ src/main/java/com/brightslearning/webblog/AdminController.java
@@ -6,7 +6,7 @@ import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
 @NoArgsConstructor
 @Controller
@@ -23,4 +23,12 @@ public class AdminController {
         model.addAttribute("users", users);
         return "userList";
     }
+
+    @PostMapping("/userlist")
+    public String refreshAdminPage(@ModelAttribute Model model){
+        Iterable<BlogUser> users = userRepo.findAll();
+        model.addAttribute("users", users);
+        return "redirect:/userList";
+    }
+
 }
